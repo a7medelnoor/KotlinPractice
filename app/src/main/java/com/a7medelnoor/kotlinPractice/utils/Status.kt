864fc18be99c10d72a3f8229d970882fdf04795c
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2020-09-13 13:56:36 +0200

    utils Status of the application

--- app/src/main/java/com/a7medelnoor/kotlinPractice/utils/Status.kt
+++ app/src/main/java/com/a7medelnoor/kotlinPractice/utils/Status.kt
@@ -1,6 +1,14 @@
 package com.a7medelnoor.kotlinPractice.utils
+/*
+*  Created By: Ahmed Elnoor
+*  Date: 9/13/2020
+*  Email: ahdnoor4@gmail.com
+*  Github: github.com/a7medelnoor
+* */
 
+// Enumerations class that hold a set of constants
 enum class Status {
+    // constants for the application status
     SUCCESS,
     LOADING,
     ERROR
