eebd46f8858bbc1afd504fb2c0e71e14bc2865f5
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2020-09-13 14:06:38 +0200

    Data class that only contains the Status class

--- app/src/main/java/com/a7medelnoor/kotlinPractice/utils/Resources.kt
+++ app/src/main/java/com/a7medelnoor/kotlinPractice/utils/Resources.kt
@@ -1,8 +1,18 @@
 package com.a7medelnoor.kotlinPractice.utils
 
+/*
+*  Created By: Ahmed Elnoor
+*  Date: 9/13/2020
+*  Email: ahdnoor4@gmail.com
+*  Github: github.com/a7medelnoor
+*/
+// data class that only contains the Status class and doesn't preform eny operation
 data class Resources<out T>(val status: Status, val data: T?, val message: String?) {
-    companion object {
 
+    // a member of a companion object declaration inside the Resources class
+    companion object {
+        // a function (Success,Loading,Error) that can be called without having a class instance but needs access
+        // to the internals of a class
         fun <T> Success(data: T?): Resources<T> {
             return Resources(Status.SUCCESS, data, null)
 
